78691008ed7a2a298c36bfdd6843ce4ec1652b29
newbee <beenewminh@outlook.com>
2023-10-15 20:56:55 +1100

    feat: add data like image and diff to crypto

--- app/routers/v1/crypto.py
+++ app/routers/v1/crypto.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from sql_app.database import DatabaseSession, engine
 from sql_app.model import Crypto, Base
 from sqlalchemy.orm import joinedload
+from datetime import datetime
 
 router = APIRouter( 
     prefix="/crypto",
@@ -13,7 +14,23 @@ Base.metadata.create_all(bind=engine)
 
 @router.get("/")
 async def get_all_cryptos(db: DatabaseSession):
-    cryptos = db.query(Crypto).options(joinedload(Crypto.values)).all()
+    raw_cryptos = db.query(Crypto).options(joinedload(Crypto.values)).all()
+
+    cryptos = []
+
+    start_id = raw_cryptos[0].values[0].id
+    cur = datetime.now().hour
+    if cur < start_id:
+        cur += 12
+    end = cur - start_id - 1
+    if end == 0:
+        end = 12
+
+    for raw_crypto in raw_cryptos:
+        raw_crypto.img = f"https://www.gemini.com/images/currencies/icons/default/{raw_crypto.name}.svg"
+        raw_crypto.diff = (raw_crypto.values[cur - start_id].value_aud - raw_crypto.values[end].value_aud) / raw_crypto.values[cur - start_id].value_aud * 100 * 100
+        cryptos.append(raw_crypto)
+
     return cryptos
 
 @router.get("/{crypto_name}")
