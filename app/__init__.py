eeb8e030128f542da889bf9951f87306f1e42501
newbee <beenewminh@outlook.com>
2023-10-15 18:48:22 +1100

    feat + fix: add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from app.routers import v1
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(v1.router)
